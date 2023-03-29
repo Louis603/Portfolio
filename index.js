bca7cfee41b9d647e0e213471ea77ad4cbdd9903
Louis Yan <louisy603@hotmail.com>
2023-03-28 22:55:00 -0400

    js loops for carousel to display dynamically

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const projects = [
             "View Site": "youtube.com"
         },
         summary: "this was made very well and i like it a lot so please like it too",
-        language: "React"
+        language: "React",
+        pics: ["pictures/tommat-1.jpeg", "pictures/tommat-2.png"]
     },
     {
         title: "Good Citizen",
@@ -17,7 +18,8 @@ const projects = [
             "View Site": "youtube.com"
         },
         summary: "i like it a lot so please like it too",
-        language: "React"
+        language: "React",
+        pics: ["pictures/good-citizen-1.png", "pictures/good-citizen-2.png" ]
     }
 ]
 
@@ -28,24 +30,32 @@ const leftTitle = document.createElement('h1')
 const description = document.createElement('p')
 const linkDiv =  document.createElement('div')
 const btnGroup = document.querySelector(".btn-group")
-let caroBtn = document.querySelector(".carousel-control-next")
+const carouselInner = document.querySelector(".carousel-inner")
+console.log(carouselInner)
+// let caroBtn = document.querySelector(".carousel-control-next")
 // caroBtn.addEventListener('click', (e) => console.log("hello"))
 // linkDiv.removeChild()
-console.log(linkDiv.childNodes)
-console.log(document.querySelector(".active"))
+// console.log(linkDiv.childNodes)
+// console.log(document.querySelector(".active"))
 
 
 const rightBox = document.getElementById("display-right")
 console.log(rightBox)
 
 const leftBox = document.getElementById("display-left")
+const leftTitleContainer = document.getElementById("display-left-title")
 // console.log(leftBox.children[0].className)
 
 function displayLeftWelcome(){
     leftTitle.textContent = "Welcome!"
     description.remove()
+
+    // while loops through the nodes of carousel and removes it
+    while(carouselInner.lastElementChild){
+        carouselInner.removeChild(carouselInner.lastElementChild)
+    }
     // linkDiv.childNodes.remove()
-    leftBox.append(leftTitle)
+    leftTitleContainer.append(leftTitle)
 }
 
 displayLeftWelcome()
@@ -69,11 +79,17 @@ function displayList(arr){
 function displayActiveLink(e,arr,title) {
     e.preventDefault()
     let currentActive = document.querySelector(".active")
+    console.log(currentActive)
     currentActive.classList.remove("active")
     title.classList.toggle("active")
-    console.log(title)
+    // console.log(title)
+
 
 }
+// function displayLeft(e,arr){
+//     e.preventDefault()
+
+// }
 
 function displayLeft(e,arr){
     e.preventDefault()
@@ -82,6 +98,11 @@ function displayLeft(e,arr){
         linkDiv.removeChild(child)
         child = linkDiv.lastElementChild
     }
+
+    while(carouselInner.lastElementChild){
+        carouselInner.removeChild(carouselInner.lastElementChild)
+    }
+    
     if(leftTitle.textContent === arr.title){
         return displayLeftWelcome()
     }
@@ -94,7 +115,19 @@ function displayLeft(e,arr){
         redirectLink.textContent = item
         linkDiv.append(redirectLink)
         leftBox.append(linkDiv)
-        console.log(linkDiv.childNodes)
+        // console.log(linkDiv.childNodes)
+    }
+    for (const item in arr.pics) {
+        console.log(arr.pics[item])
+        let caroDiv = document.createElement('div')
+        let caroImg = document.createElement('img')
+        caroImg.src = arr.pics[item]
+        caroImg.classList.add('d-block','w-100')
+        console.log(arr.pics[0] === arr.pics[item] ? true : false)
+        arr.pics[0] === arr.pics[item] ? caroDiv.classList.add('carousel-item', 'active') : caroDiv.classList.add('carousel-item')
+        caroDiv.append(caroImg)
+        carouselInner.append(caroDiv)
+        // carouselInner.append("skaljdfkls")
     }
     leftBox.append(description)
 }
